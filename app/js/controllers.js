b1fe9dc2f8f1c219948ba940153713fe7b1984a7
Leo Dirac <leo@scaledrecognition.com>
2013-11-27 21:43:33 -0800

    Repeated timer.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,9 +5,11 @@
 angular.module('myApp.controllers', []).
   controller('MyCtrl1', ['$scope','$timeout',function($scope,$timeout) {
     $scope.displaytime= "8:30:11 PM";
-    $timeout(function() {
+    function tick() {
         $scope.displaytime += "foo";
-    },2000);
+        $timeout(tick, 2000);
+    };
+    tick();
   }])
   .controller('MyCtrl2', [function() {
 
