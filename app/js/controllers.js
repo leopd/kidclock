88c8946be5634dc73c66c6e3915da472c266037e
Leo Dirac <leo@scaledrecognition.com>
2013-11-27 21:35:55 -0800

    Time string rendered in controller.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,8 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope',function($scope) {
+    $scope.displaytime= "8:30:11 PM";
   }])
   .controller('MyCtrl2', [function() {
 
