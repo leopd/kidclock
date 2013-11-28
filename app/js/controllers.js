41da119cbc646dfdbc2ff2c732bf1e6e4e5a534b
Leo Dirac <leo@scaledrecognition.com>
2013-11-27 21:42:22 -0800

    POC of using timer.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,11 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope',function($scope) {
+  controller('MyCtrl1', ['$scope','$timeout',function($scope,$timeout) {
     $scope.displaytime= "8:30:11 PM";
+    $timeout(function() {
+        $scope.displaytime += "foo";
+    },2000);
   }])
   .controller('MyCtrl2', [function() {
 
