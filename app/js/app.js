bcc0b4663ad05d3bb5f5dcb4760ad55eaa4aa5a5
Leo Dirac <leo@scaledrecognition.com>
2013-11-28 19:02:26 -0800

    datepicker working in this really hacked up mess.

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('kidClock', [
-  'ngRoute',
+  //'ngRoute',
   '$strap.directives',
   'kidClock.filters',
   'kidClock.services',
