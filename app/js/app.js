e2bba5b1e706c1abf20fbda37a26b083b2b218bc
Leo Dirac <leo@scaledrecognition.com>
2014-09-11 20:50:33 -0700

    Work in progress repairing.

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('kidClock', [
   //'ngRoute',
-  //'$strap.directives',
+  '$strap.directives',
   'kidClock.filters',
   'kidClock.services',
   'kidClock.directives',
